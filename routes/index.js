ca8182a8cde0de7ef5dabd7bcbebccaff47cd3f1
redtree0 <redtree941@naver.com>
2018-10-12 20:09:48 +0000

    testset

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,9 @@ router.get('/test', function(req, res, next) {
   res.render('topology-graph', {title : 'express'})
 });
 var controller = require('../controller');
-router.get('/k8s', function(req, res, next) {
+router.get('/k8s.jsonp', function(req, res, next) {
     controller.getListk8s((k8s)=>{
-      res.json(k8s);
+      res.send('sink('+JSON.stringify(k8s)+')');
     });
 });
 module.exports = router;
