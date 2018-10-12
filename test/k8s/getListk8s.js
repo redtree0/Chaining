2bc4511ed28989caba1dbb45617cb4add07c80e8
김천규 <redtree941@naver.com>
2018-10-13 02:42:53 +0900

    add topology-page

---
+++ test/k8s/getListk8s.js
@@ -0,0 +1,21 @@
+const Client = require('kubernetes-client').Client;
+const config = require('kubernetes-client').config;
+
+const client = new Client({ config: config.fromKubeconfig(), version: '1.9' });
+
+async function main() {
+    const deployment = await client.apis.apps.v1.namespaces('redtree0').deployments('redtree0-ide').get();
+
+
+    // console.log(deployment.body.spec.template.spec.containers[0].ports[0]);
+    console.log(deployment.body.spec.template.spec.containers[0].ports[0].hostPort);
+    var port = (deployment.body.spec.template.spec.containers[0].ports[0].hostPort);
+    const pod = await client.api.v1.namespaces('redtree0').pods.get('redtree0');
+    console.log(pod.body.items[0].status.hostIP)
+    var ns = await client.api.v1.namespaces.get();
+    console.log(ns)
+}
+
+main()
+
+
