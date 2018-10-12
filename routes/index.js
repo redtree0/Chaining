9db56bd2366328f63313077c826de224a5705b6f
김천규 <redtree941@naver.com>
2018-10-13 02:41:54 +0900

    add testpage

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title : 'express'})
 });
-
+router.get('/test', function(req, res, next) {
+  res.render('topology-graph', {title : 'express'})
+});
 module.exports = router;
