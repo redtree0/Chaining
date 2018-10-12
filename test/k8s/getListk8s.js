d0e39b74e46eb46c207a0aaa5859f60f36eb8045
redtree0 <redtree941@naver.com>
2018-10-12 17:59:54 +0000

    update getListk8s

--- test/k8s/getListk8s.js
+++ test/k8s/getListk8s.js
@@ -8,12 +8,28 @@ async function main() {
 
 
     // console.log(deployment.body.spec.template.spec.containers[0].ports[0]);
-    console.log(deployment.body.spec.template.spec.containers[0].ports[0].hostPort);
+    //console.log(deployment.body.spec.template.spec.containers[0].ports[0].hostPort);
     var port = (deployment.body.spec.template.spec.containers[0].ports[0].hostPort);
     const pod = await client.api.v1.namespaces('redtree0').pods.get('redtree0');
-    console.log(pod.body.items[0].status.hostIP)
+    //console.log(pod.body.items[0].status.hostIP)
     var ns = await client.api.v1.namespaces.get();
-    console.log(ns)
+    //console.log(ns.body.items)
+    var myns = ns.body.items.filter((item)=>{
+	    return (item.metadata.name != 'kube-system' && item.metadata.name != 'kube-public')
+    })
+	//console.log(myns);
+    myns = myns.map((item)=>{
+	    return item.metadata.name
+    })
+	console.log(myns);
+	const promises = myns.map(getPod);
+	await Promise.all(promises);
+
+}
+async function getPod(ns){
+
+    var pod = await client.api.v1.namespaces(ns).pods.get();
+    console.log(pod.body.items[0].status)
 }
 
 main()
