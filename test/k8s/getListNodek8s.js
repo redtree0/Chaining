022b929b2a01d538b7c10fab653a1518d886e07c
김천규 <redtree941@naver.com>
2018-10-14 09:17:41 +0900

    add k8s example

---
+++ test/k8s/getListNodek8s.js
@@ -0,0 +1,40 @@
+const Client = require('kubernetes-client').Client;
+const config = require('kubernetes-client').config;
+
+const client = new Client({ config: config.fromKubeconfig(), version: '1.9' });
+
+async function main() {
+    const nodes = await client.apis.v1.nodes.get();
+
+    console.log(nodes);
+    // // console.log(deployment.body.spec.template.spec.containers[0].ports[0]);
+    // //console.log(deployment.body.spec.template.spec.containers[0].ports[0].hostPort);
+    // const pod = await client.api.v1.namespaces('redtree0').pods.get('redtree0');
+    // //console.log(pod.body.items[0].status.hostIP)
+    // var ns = await client.api.v1.namespaces.get();
+    // //console.log(ns.body.items)
+    // var myns = ns.body.items.filter((item)=>{
+	//     return (item.metadata.name != 'kube-system' && item.metadata.name != 'kube-public')
+    // })
+	// //console.log(myns);
+    // myns = myns.map((item)=>{
+	//     return item.metadata.name
+    // })
+	// console.log(myns);
+
+	// const promises = myns.map(getPod);
+	// await Promise.all(promises).then(function(val){
+	// 	console.log(val);
+	// });
+
+}
+// async function getPod(ns){
+
+//     var pod = await client.api.v1.namespaces(ns).pods.get();
+//     //console.log(pod.body.items[0].status)
+// return 	Promise.resolve(pod)
+// }
+
+main()
+
+
