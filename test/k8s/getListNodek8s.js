b7fe4115c3ebe3287f1ddd46ad2b5a2f487acd5f
redtree0 <redtree941@naver.com>
2018-10-14 00:42:09 +0000

    update k8s test code

--- test/k8s/getListNodek8s.js
+++ test/k8s/getListNodek8s.js
@@ -8,25 +8,10 @@ async function main() {
 
     var data ={}
     nodes.body.items.forEach(v=>{
-
-        json = Object.assign({}, v.items);
-    
-        json = v.items.reduce((json, value, key) => { json[key] = value; return json; }, {});
-        delete v['items']
-
-        for(var i in json){
-            var tmp = json[i];
-            console.log(tmp)
-            // tmp.kind = v.kind;
-            tmp.kind = "Node";
-            tmp.apiVersion = v.apiVersion;
-            tmp.metadata = v.metadata;
-            key = tmp.metadata.uid || key 
-            data[key] = tmp; 
-
-        }
+	var key = v.metadata.uid || key
+	data[key] = v    
     })
-
+    console.log(data)
 }
 
 main()
